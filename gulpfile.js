bc262f5986e61bacf20809ad9416b1cc4b8e879f
Jeff <jeffhood@gmail.com>
2016-10-02 19:59:15 -0500

    Added src, pipe, and dest to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+
+    watch = require('gulp-watch');
 
 gulp.task('default', function() {
      
@@ -10,3 +12,18 @@ gulp.task('default', function() {
 gulp.task("html", function(){
 	console.log("Imagine something useful being done to your HTML");
 });
+
+gulp.task("styles", function(){
+	return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task("watch", function(){
+
+	watch('./app/index.html', function(){
+		gulp.start('html');
+	});
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('styles');
+	});
+});
+
