68851ff51f42ef8b5bbf05245205b19fa076dea0
Jeff <jeffhood@gmail.com>
2016-10-21 05:39:37 -0500

    added styles.js and watch.js to clean up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,14 @@
-var gulp = require('gulp'),
-
-    watch = require('gulp-watch'),
-
-    postcss = require('gulp-postcss'),
-
-    autoprefixer = require('autoprefixer'),
-
-    cssvars = require('postcss-simple-vars'),
-
-    nested = require('postcss-nested'),
-
-    cssImport = require('postcss-import'),
-
-    browserSync = require('browser-sync').create();
-
-gulp.task('default', function() {
-     
-     console.log("Yes - you created a Gulp task");
-
-	});
+require('./gulp/task/styles');
+require('./gulp/task/watch');
+    
+    
 
 
-gulp.task("html", function(){
-	console.log("Imagine something useful being done to your HTML");
-});
 
-gulp.task("styles", function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task("watch", function(){
 
-	browserSync.init({
-		server: {
-			baseDir: "app"
-		}
-	});
 
-	watch('./app/index.html', function(){
-		gulp.start('html');
-	});
-	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('styles');
-	});
-});
+
+
+
 
